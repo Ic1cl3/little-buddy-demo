e2aff5372607c8a5c669bced23439f11f66cf696
Natnael Yohannes <natnael.m.yohannes@gmail.com>
2026-06-29 15:51:43 -0400

    fixed timeBug

--- Desktop/Desktop.gd
+++ Desktop/Desktop.gd
@@ -18,7 +18,10 @@ func _process(_delta: float) -> void:
 	# Assign time label
 	var currentTime = Time.get_datetime_dict_from_system()
 	if normalTime:
-		Clock.text = str(((currentTime["hour"] - 1) % 12) + 1) + ":" + str(currentTime["minute"])
+		var minuteString = str(currentTime["minute"])
+		if len(minuteString) < 2:
+			minuteString = "0" + minuteString
+		Clock.text = str(((currentTime["hour"] - 1) % 12) + 1) + ":" + minuteString
 
 
 func _on_texture_button_pressed() -> void:
